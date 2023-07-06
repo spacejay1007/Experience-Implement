7e143404d84fcde21de1ce155a174423bedbb4c9
kjy <spacejay1007@gmail.com>
2023-07-06 21:04:31 +0900

    axios

--- Axios/components/index.js
+++ Axios/components/index.js
@@ -4,18 +4,21 @@ var axios_1 = require("axios"); //  Module -> index.d.ts // export default axios
 var axios = axios_1["default"].create({
     headers: { "Content-Type": "application/json" }
 });
-// Request > interceptor 에 추가
+// Request > interceptor
 axios.interceptors.request.use(function (config) {
     // console.log()
-    console.log(config);
+    console.log(config); // config 가 어찌 되어있는 보고 싶다
     return config;
 }, function (err) {
-    console.log(err);
+    console.log("Request Err : ".concat(err));
 });
-// response > interceptor 에 추가
+// response interceptor
 axios.interceptors.response.use(function (res) {
     return res;
-}, function (err) { return console.log(err); });
-axios
-    .get("https://reqres.in/api/users?page=2")
-    .then(function (res) { return console.log(res.data); });
+}, function (err) {
+    console.log("Response Err : ".concat(err));
+});
+axios.get("https://reqres.in/api/users?page=2").then(function (res) {
+    console.log(res.data);
+    return res;
+});
