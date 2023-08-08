79463d2e115fa4b6edb3beba54ea7d6d0183334a
kjy <spacejay1007@gmail.com>
2023-08-08 21:24:24 +0900

    dragIntercal

--- Scroll/DragInterval/dragInterval.js
+++ Scroll/DragInterval/dragInterval.js
@@ -1,17 +1,33 @@
 var dgBox = document.querySelector(".drag__box");
+var state = [];
+var eventFlag = false;
 var dataArr = new Array(24).fill(0).map(function (item, idx) {
     var itemTag = document.createElement("div");
     itemTag.textContent = "".concat(idx);
     itemTag.className = "iterval__box";
     dgBox === null || dgBox === void 0 ? void 0 : dgBox.appendChild(itemTag);
-    itemTag.addEventListener("mouseleave", function () {
-        console.log("mouseLeave", idx);
+    // itemTag.addEventListener("mouseleave", () => {
+    //   console.log("mouseLeave", idx);
+    // });
+    // itemTag.addEventListener("mousemove", () => {
+    //   console.log("mouseMove", idx);
+    // });
+    // itemTag.addEventListener("mouseout", () => {
+    //   console.log("mouseOut", idx);
+    // });
+    itemTag.addEventListener("mousedown", function () {
+        console.log("moseDown", idx);
+        eventFlag = true;
     });
     itemTag.addEventListener("mousemove", function () {
-        console.log("mouseMove", idx);
+        // console.log("mouseMove", idx);
+        if (eventFlag) {
+            console.log(idx);
+        }
     });
-    itemTag.addEventListener("mouseout", function () {
-        console.log("mouseOut", idx);
+    itemTag.addEventListener("mouseup", function () {
+        console.log("mouseup", idx);
+        eventFlag = false;
     });
 });
 console.log(dataArr);
--- Scroll/DragInterval/dragInterval.ts
+++ Scroll/DragInterval/dragInterval.ts
@@ -1,19 +1,33 @@
 const dgBox = document.querySelector(".drag__box");
+const state: any[] = [];
+let eventFlag = false;
 const dataArr = new Array(24).fill(0).map((item, idx) => {
   let itemTag = document.createElement("div")!;
   itemTag.textContent = `${idx}`;
   itemTag.className = `iterval__box`;
   dgBox?.appendChild(itemTag);
 
-  itemTag.addEventListener("mouseleave", () => {
-    console.log("mouseLeave", idx);
+  // mouseDown(마우스 왼쪽버튼을 클릭 하고 있을 때)을 하여 eventFlag 를 true 로 만들고
+  itemTag.addEventListener("mousedown", () => {
+    console.log("moseDown", idx);
+    eventFlag = true;
   });
+
+  // mouseDown 이 되는 동안(eventFlag 가 true 인 동안) idx를 담아준다.
   itemTag.addEventListener("mousemove", () => {
-    console.log("mouseMove", idx);
+    // console.log("mouseMove", idx);
+    if (eventFlag) {
+      state.push(idx);
+    }
+    console.log(state);
   });
-  itemTag.addEventListener("mouseout", () => {
-    console.log("mouseOut", idx);
+  // mouseUp(마우스 왼쪽버튼을 떼어냈을 때)을 하여 eventFlag 를 false 로 만들고
+
+  itemTag.addEventListener("mouseup", () => {
+    console.log("mouseup", idx);
+    eventFlag = false;
   });
 });
+
 console.log(dataArr);
 // divElement?.id = dataArr;
