d5fa3ad53383a89ee1d10d0d9de7f0226d3d3fe5
kjy <spacejay1007@gmail.com>
2023-09-05 15:54:18 +0900

    progressBar && event Study

--- Scroll/getBoundingClientRect/getBoundingClientRect.ts
+++ Scroll/getBoundingClientRect/getBoundingClientRect.ts
@@ -1,3 +1,4 @@
+// 태그 정보가 있어야 한다. React에서는 ref.current 가 예시가 된다.
 // tag 의 크기의 높이 or width 등 의 상대적인 위치 정보를 제공
 const tag = document.querySelector(".tagname");
 const tagHeight = tag?.getBoundingClientRect();
